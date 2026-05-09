e8ee99f752974d562c37d4c076587cf86f7d124d
AlerickL <alericklanglois@gmail.com>
2026-05-09 15:45:43 -0400

    Add ISBN verification

--- src/main/java/org/alerick/Book.java
+++ src/main/java/org/alerick/Book.java
@@ -6,7 +6,24 @@ public class Book extends Item {
 
     public Book(String title, Status status, String author, String ISBN, String genre) {
         super(title, status, author);
-        this.ISBN = ISBN;
+        this.ISBN = verifyISBN(ISBN);
         this.genre = genre;
+    }
+
+    /**
+     * checks the validity of the ISBN
+     * @param str the ISBN to be checked
+     * @return INVALID if the ISBN does not follow adequate formatting, or the initial ISBN if it's correct
+     */
+    public static String verifyISBN(String str) {
+        if (str.length() != 12 ) {
+            return "INVALID";
+        }
+        for (Character c : str.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return "INVALID";
+            }
+        }
+        return str;
     }
 }
