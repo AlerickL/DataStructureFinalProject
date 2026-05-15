a8b19808e5c834e9a6cc9304e245c82a7bf6780d
AlerickL <alericklanglois@gmail.com>
2026-05-14 21:06:14 -0400

    change names

--- src/main/java/org/alerick/Constants.java
+++ src/main/java/org/alerick/Constants.java
@@ -3,6 +3,6 @@ package org.alerick;
 public class Constants {
     public static final int STUDENTLIMIT = 5;
     public static final int TEACHERLIMIT = 10;
-    public static final String BOOKS_CSV_PATH = "src/main/resources/books.csv";
+    public static final String ITEMS_CSV_PATH = "src/main/resources/items.csv";
     public static final String USERS_CSV_PATH = "src/main/resources/users.csv";
 }
