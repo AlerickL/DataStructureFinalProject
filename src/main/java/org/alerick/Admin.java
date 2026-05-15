f33514c3df3e2ac3da96accfb9c7d65cb2acd1ba
AlerickL <alericklanglois@gmail.com>
2026-05-14 22:02:44 -0400

    add report

--- src/main/java/org/alerick/Admin.java
+++ src/main/java/org/alerick/Admin.java
@@ -2,24 +2,42 @@ package org.alerick;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Admin extends User {
+public class Admin extends User implements Reportable {
 
     public Admin(String name) {
         super(name);
         this.borrowingLimit = Integer.MAX_VALUE;
     }
 
-    public static List<Item> report(SearchType searchType, String string) {
-        return List.of();
+    public String report() {
+        File file = new File(Constants.ITEMS_CSV_PATH);
+        StringBuilder items = new StringBuilder();
+        try (Scanner sc = new Scanner(file)) {
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String[] elements = line.split(",");
+                items.append(elements[2])
+                        .append(": ")
+                        .append(line)
+                        .append("\n");
+            }
+            return items.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
      * Backs up current Items and Users into two CSV files
      */
     public static void backup() {
+        User.backUpUsers();
+        Item.backUpItems();
     }
 
     @Override
